1c27ad8a400619d5e17c63f9ec6232dcc7274a91
muraoka naohito <ciel.phantomhive.4869@gmail.com>
2021-02-17 23:31:16 +0900

    add: Scikit learn for taking missing mean value

--- data_pre_processing.py
+++ data_pre_processing.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
+from sklearn.impute import SimpleImputer
+
 # dependent variable - 従属変数 - ある要因によって影響された結果として表れる変数のことを目的変数あるいは従属変数
 # something that we wanna predict
 
@@ -12,6 +14,10 @@ dataset = pd.read_csv("Data.csv")
 # matrics of feature - : will take all the rows, -1 means index of last column
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
 
 print(x)
 print(y)
