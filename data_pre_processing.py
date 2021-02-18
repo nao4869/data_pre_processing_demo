8c6a36983c88511d36a220de83640b4990141c95
muraoka naohito <ciel.phantomhive.4869@gmail.com>
2021-02-18 22:42:11 +0900

    add: encoding categorical data, dependent variable

--- data_pre_processing.py
+++ data_pre_processing.py
@@ -4,9 +4,11 @@ import pandas as pd
 import os
 
 from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 
 # dependent variable - 従属変数 - ある要因によって影響された結果として表れる変数のことを目的変数あるいは従属変数
-# something that we wanna predict
 
 # read the dataset from CSV file
 dataset = pd.read_csv("Data.csv")
@@ -18,6 +20,20 @@ y = dataset.iloc[:, -1].values
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+# Encoding categorical data
+columnTransformer = ColumnTransformer(
+    transformers=[
+        ('encoder', OneHotEncoder(), [0])
+    ],
+    remainder='passthrough',
+)
+
+x = np.array(columnTransformer.fit_transform(x))
+
+# Encoding the dependent variable
+labelEncoder = LabelEncoder()
+y = labelEncoder.fit_transform(y)
 
 print(x)
 print(y)
