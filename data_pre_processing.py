ca3e4c3898d94d55813f7fe63e2694c09f0181e8
muraoka naohito <ciel.phantomhive.4869@gmail.com>
2021-02-19 22:17:29 +0900

    add: feature scaling

--- data_pre_processing.py
+++ data_pre_processing.py
@@ -7,6 +7,9 @@ from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
 
 # dependent variable - 従属変数 - ある要因によって影響された結果として表れる変数のことを目的変数あるいは従属変数
 
@@ -24,10 +27,7 @@ x[:, 1:3] = imputer.transform(x[:, 1:3])
 # Encoding categorical data
 columnTransformer = ColumnTransformer(
     transformers=[
-        ('encoder', OneHotEncoder(), [0])
-    ],
-    remainder='passthrough',
-)
+        ('encoder', OneHotEncoder(), [0])], remainder='passthrough')
 
 x = np.array(columnTransformer.fit_transform(x))
 
@@ -35,5 +35,23 @@ x = np.array(columnTransformer.fit_transform(x))
 labelEncoder = LabelEncoder()
 y = labelEncoder.fit_transform(y)
 
-print(x)
-print(y)
+# Splitting the dataset into Training set and Test set
+x_train, x_test, y_train, y_test = train_test_split(
+    x,
+    y,
+    test_size=0.2,
+    random_state=1,
+)
+
+# Feature Scalling
+scaller = StandardScaler()
+x_train[:, 3:] = scaller.fit_transform(x_train[:, 3:])
+x_test[:, 3:] = scaller.transform(x_test[:, 3:])
+
+# print(x)
+# print(y)
+
+print(x_train)
+print(x_test)
+# print(y_train)
+# print(y_test)
